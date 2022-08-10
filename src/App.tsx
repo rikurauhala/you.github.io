be5975d423dddd4765ddccb22f49debeeb32cda2
Riku Rauhala <riku@rauhala.io>
2022-08-10 19:38:09 +0300

    Modify App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,8 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello there!
     </div>
   );
 };
