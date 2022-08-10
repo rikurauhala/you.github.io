06fe55ebc94cf701444e48c6ecbf435136113324
Riku Rauhala <riku@rauhala.io>
2022-08-10 20:49:54 +0300

    Create component About

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Info from './components/Info';
+import About from './components/About';
 
 import './App.css';
 
@@ -6,6 +7,7 @@ const App = () => {
   return (
     <div>
       <Info />
+      <About />
     </div>
   );
 };
---
+++ src/components/About.tsx
@@ -0,0 +1,22 @@
+const About = () => {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>
+        Hello there! My name is Riku. Welcome to my professional home page.
+      </p>
+      <p>
+        I am a computer science student at the <a href="https://www.helsinki.fi/" target="_blank">
+        University of Helsinki</a>. This summer I'm working as a trainee at&nbsp;
+        <a href="https://www.if-insurance.com/about-if" target="_blank">If Insurance</a>.
+        I'm also learning modern full stack development with the course&nbsp;
+        <a href="https://fullstackopen.com/en/" target="_blank">Full stack open</a>!
+      </p>
+      <p>
+        To contact me, please send me an email. You can find the address on my GitHub page.
+      </p>
+    </div>
+  );
+};
+
+export default About;
