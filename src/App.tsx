0d77925eb8fba2898ea5f124891dc33c89b8ccf7
Riku Rauhala <riku@rauhala.io>
2022-08-12 22:41:56 +0300

    Sort repositories by the date of the latest commit

--- src/App.tsx
+++ src/App.tsx
@@ -25,9 +25,13 @@ const App = (): JSX.Element => {
       html_url: repository.html_url,
       id: repository.id,
       name: repository.name,
+      pushed_at: new Date(repository.pushed_at),
       topics: repository.topics,
       year: repository.created_at.substring(0,4)
-    }));
+    }))
+    .sort((a, b) => b.pushed_at.getTime() - a.pushed_at.getTime());
+  
+  console.log(filteredRepositories);
 
   const octokit = new Octokit();
   const username = 'rikurauhala';
--- src/types/types.ts
+++ src/types/types.ts
@@ -12,6 +12,7 @@ export interface Repository {
   html_url: string
   id: number
   name: string
+  pushed_at: Date
   topics: Array<string>
   year: string
 }
