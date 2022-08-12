ee66672b4025db0584431c110178fb449b13ea90
Riku Rauhala <riku@rauhala.io>
2022-08-12 15:29:14 +0300

    Include only projects marked as part of the portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -17,15 +17,17 @@ import './App.css';
 
 const App = (): JSX.Element => {
   const [repositories, setRepositories] = useState<Array<RepositoryFull>>([]);
-  const filteredRepositories: Array<Repository> = repositories.map(repository => ({
-    created_at: repository.created_at,
-    description: repository.description,
-    homepage: repository.homepage,
-    html_url: repository.html_url,
-    id: repository.id,
-    name: repository.name,
-    topics: repository.topics,
-  }));
+  const filteredRepositories: Array<Repository> = repositories
+    .filter(repository => !repository.fork && repository.topics.includes('portfolio'))
+    .map(repository => ({
+      created_at: repository.created_at,
+      description: repository.description,
+      homepage: repository.homepage,
+      html_url: repository.html_url,
+      id: repository.id,
+      name: repository.name,
+      topics: repository.topics,
+    }));
 
   const octokit = new Octokit();
   const username = 'rikurauhala';
