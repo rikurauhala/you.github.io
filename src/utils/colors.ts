f76db442c7c020df06eadb5e44e7d7070f6323de
Riku Rauhala <riku@rauhala.io>
2022-08-13 18:55:24 +0300

    Add more color hex codes

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -1,8 +1,10 @@
 export const colors = {
   'CSS': '#563d7c',
+  'Dockerfile': '#384d54',
   'HTML': '#e34c26',
   'Java': '#b07219',
   'JavaScript': '#f1e05a',
+  'Procfile': '#3b2f63',
   'Python': '#3572a5',
   'Shell': '#89e051',
   'TypeScript': '#3178c6'
