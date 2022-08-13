561199e3c9c6ccf2388422d507b4b7c30b00cc8f
Riku Rauhala <riku@rauhala.io>
2022-08-13 18:55:08 +0300

    Improve footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,6 +3,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <div className='footer'>
+      <hr/>
       rauhala.io | Riku Rauhala | 2022 |&nbsp;
       <a href="https://github.com/rikurauhala/rikurauhala.github.io">Source code</a>
     </div>
