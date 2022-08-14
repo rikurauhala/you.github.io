d51cc6d6b755a7136c6f42a417b0d7d36c00e4e4
Riku Rauhala <riku@rauhala.io>
2022-08-14 14:04:23 +0300

    Create type User based on UserFull

--- src/types/types.ts
+++ src/types/types.ts
@@ -99,31 +99,28 @@ export interface RepositoryFull {
   web_commit_signoff_required: boolean
 }
 
-export interface User {
-  avatar_url: string
-  events_url: string
-  followers_url: string
-  following_url: string
-  gists_url: string
-  gravatar_id: string
-  html_url: string
-  id: number
-  login: string
-  node_id: string
-  organizations_url: string
-  received_events_url: string
-  repos_url: string
-  site_admin: boolean
-  starred_url: string
-  subscriptions_url: string
-  type: string
-  url: string
-}
+type ToBeOmitted = 
+  | 'bio'
+  | 'blog'
+  | 'company'
+  | 'created_at'
+  | 'email'
+  | 'followers'
+  | 'following'
+  | 'hireable'
+  | 'location'
+  | 'name'
+  | 'public_gists'
+  | 'public_repos'
+  | 'twitter_usename'
+  | 'updated_at';
+
+export type User = Omit<UserFull, ToBeOmitted>;
 
 export interface UserFull {
   avatar_url: string
-  blog: string
   bio: string
+  blog: string
   company: string
   created_at: string
   email: string
