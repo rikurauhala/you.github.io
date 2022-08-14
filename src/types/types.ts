5f7ef6bc08c52445b8807d9eceddbaa8b2363dbf
Riku Rauhala <riku@rauhala.io>
2022-08-14 13:08:09 +0300

    Create type UserFull

--- src/types/types.ts
+++ src/types/types.ts
@@ -119,3 +119,38 @@ export interface User {
   type: string
   url: string
 }
+
+export interface UserFull {
+  avatar_url: string
+  blog: string
+  bio: string
+  company: string
+  created_at: string
+  email: string
+  events_url: string
+  followers: number
+  followers_url: string
+  following: number
+  following_url: string
+  gists_url: string
+  gravatar_id: string
+  hireable: boolean
+  html_url: string
+  id: number
+  location: string
+  login: string
+  name: string
+  node_id: string
+  organizations_url: string
+  public_gists: number
+  public_repos: number
+  received_events_url: string
+  repos_url: string
+  site_admin: boolean
+  starred_url: string
+  subscriptions_url: string
+  twitter_username: string
+  type: string
+  updated_at: string
+  url: string 
+}
