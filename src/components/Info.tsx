84373efef91e627ffb7230ac20713df0dc260e6a
Riku Rauhala <riku@rauhala.io>
2022-08-14 11:47:07 +0300

    Refactor Info.tsx into components

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,3 +1,5 @@
+import { Link, Typography } from '@mui/material';
+
 import profilePicture from '../assets/images/riku.jpg';
 
 import ageIcon from '../assets/icons/details/age.svg';
@@ -9,43 +11,73 @@ import liIcon from '../assets/icons/social/li.svg';
 
 import './Info.css';
 
-const Info = () => {
-  return (
-    <div className='info'>
-      <img
-        alt='Profile picture'
-        className='picture'
-        src={profilePicture as string}
-        title='This is me!'
-      /> 
-      <h1>Riku Rauhala</h1>
-      <p className='title'>
-        Computer science student at&nbsp;
-        <a href='https://www.helsinki.fi/' target='_blank'>
-          University of Helsinki
-        </a>
-      </p>
-      <p className='details'>
-        <img src={ageIcon as string} className='icon-info' title='Age' /> 26 |&nbsp;
-        <img src={sexIcon as string} className='icon-info' title='Sex' /> M |&nbsp;
-        <img src={locIcon as string} className='icon-info' title='Location' /> Finland
-      </p>
-      <a href='https://github.com/rikurauhala' className='link-social' target='_blank'>
-        <img
-          className='icon-social'
-          src={ghIcon as string}
-          title='See my code on GitHub'
-        />
-      </a>
-      <a href='https://www.linkedin.com/in/rikurauhala/' className='link-social' target='_blank'>
-        <img
-          className='icon-social'
-          src={liIcon as string}
-          title='Connect with me on LinkedIn'
-        />
-      </a>
-    </div>
-  );
-};
+const ProfilePicture = (): JSX.Element => (
+  <img
+    alt='Profile picture'
+    className='picture'
+    src={profilePicture as string}
+    title='This is me!'
+  />
+);
+
+const Name = (): JSX.Element => (
+  <Typography variant='h4' component='h1'>
+    Riku Rauhala
+  </Typography>
+);
+
+const Title = (): JSX.Element => (
+  <p className='title'>
+    <Typography variant='subtitle1' component='h2'>
+      Computer science student at&nbsp;
+      <Link href='https://www.helsinki.fi/' target='_blank'>
+        University of Helsinki
+      </Link>
+    </Typography>
+  </p>
+);
+
+const Details = (): JSX.Element => (
+  <p className='details'>
+    <img src={ageIcon as string} className='icon-info' title='Age' /> 26 |&nbsp;
+    <img src={sexIcon as string} className='icon-info' title='Sex' /> M |&nbsp;
+    <img src={locIcon as string} className='icon-info' title='Location' /> Finland
+  </p>
+);
+
+const Socials = (): JSX.Element => (
+  <>
+    <Social 
+      url='https://github.com/rikurauhala'
+      icon={ghIcon as string}
+      title='See my code on GitHub'
+    />
+    <Social
+      url='https://www.linkedin.com/in/rikurauhala/'
+      icon={liIcon as string}
+      title='Connect with me on LinkedIn'
+    />
+  </>
+);
+
+const Social = ({ url, icon, title }: { url: string, icon: string, title: string }): JSX.Element => (
+  <Link href={url} className='link-social' target='_blank'>
+    <img
+      className='icon-social'
+      src={icon}
+      title={title}
+    />
+  </Link>
+);
+
+const Info = (): JSX.Element => (
+  <div className='info'>
+    <ProfilePicture />
+    <Name />
+    <Title />
+    <Details />
+    <Socials />
+  </div>
+);
 
 export default Info;
