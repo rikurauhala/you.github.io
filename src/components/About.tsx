b82b29b12a4b946946a1523df85979c6c5c9c284
Riku Rauhala <riku@rauhala.io>
2022-08-14 19:58:52 +0300

    Refactor About.tsx into components

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,21 +1,26 @@
+import { Link, Typography } from '@mui/material';
+
+import Subtitle from './Subtitle';
+import Paragraph from './Paragraph';
+
 const About = () => {
+  const subtitle = 'About';
+  const paragraph1 = 'Hello there! My name is Riku. Welcome to my professional home page.';
+  const paragraph3 = 'To contact me, send me an email. You can find the address on my GitHub page.';
+
   return (
-    <div>
-      <h2>About</h2>
-      <p>
-        Hello there! My name is Riku. Welcome to my professional home page.
-      </p>
-      <p>
-        I am a computer science student at the <a href='https://www.helsinki.fi/' target='_blank'>
-        University of Helsinki</a>. This summer I'm working as a trainee at&nbsp;
-        <a href='https://www.if-insurance.com/about-if' target='_blank'>If Insurance</a>.
+    <>
+      <Subtitle subtitle={subtitle} />
+      <Paragraph content={paragraph1} />
+      <Typography paragraph component='p' variant='body1'>
+        I am a computer science student at the <Link href='https://www.helsinki.fi/'>
+        University of Helsinki</Link>. This summer I'm working as a trainee at&nbsp;
+        <Link href='https://www.if-insurance.com/about-if'>If Insurance</Link>.
         I'm also learning modern full stack development with the course&nbsp;
-        <a href='https://fullstackopen.com/en/' target='_blank'>Full stack open</a>!
-      </p>
-      <p>
-        To contact me, please send me an email. You can find the address on my GitHub page.
-      </p>
-    </div>
+        <Link href='https://fullstackopen.com/en/'>Full stack open</Link>!
+      </Typography>
+      <Paragraph content={paragraph3} />
+    </>
   );
 };
 
