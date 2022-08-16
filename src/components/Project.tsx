71ff5fe43ba5c9674c9b39f4ec918bfe4716aa79
Riku Rauhala <riku@rauhala.io>
2022-08-16 16:41:19 +0300

    Refactor Project.tsx into components

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -2,31 +2,47 @@ import { Card, CardContent, Grid, Link, Typography } from '@mui/material';
 import { Repository } from '../types/types';
 import LanguageBar from './LanguageBar';
 
+const ProjectName = ({ url, name }: { url: string, name: string }): JSX.Element => (
+  <Link href={url}>
+    <Typography gutterBottom variant='h6' component='h2'>
+      {name}
+    </Typography>
+  </Link>
+);
+
+const ProjectYear = ({ year }: { year: string }): JSX.Element => (
+  <Typography component='p' style={{color: '#5d7275', marginBottom: '8px'}}>
+    {year}
+  </Typography>
+);
+
+const ProjectDescription = ({ description }: { description: string }): JSX.Element => (
+  <Typography variant='body2' component='p' style={{color: '#fff'}}>
+    {description}
+  </Typography>
+);
+
+const ProjectTopics = ({ topics }: { topics: Array<string> }): JSX.Element => (
+  <Typography variant='body2' component='p' style={{color: '#5e707a'}}>
+    {topics
+      .filter(topic => topic !== 'portfolio')
+      .map(topic =>
+        <span key={topic}>#{topic} </span>
+      )}
+  </Typography>
+);
+
 const Project = ({ repository }: { repository: Repository }): JSX.Element => (
   <Grid item xs={12} md={6} style={{display: 'flex'}}>
     <Card style={{backgroundColor: '#051222', width: '100%', border: '#0f3552 1px solid'}}>
       <CardContent>
-        <Link href={repository.html_url}>
-          <Typography gutterBottom variant='h6' component='h2'>
-            {repository.name}
-          </Typography>
-        </Link>
-        <Typography component='p' style={{color: '#5d7275', marginBottom: '8px'}}>
-          {repository.year}
-        </Typography>
-        <Typography variant='body2' component='p' style={{color: '#fff'}}>
-          {repository.description}
-        </Typography>
+        <ProjectName url={repository.html_url} name={repository.name} />
+        <ProjectYear year={repository.year} />
+        <ProjectDescription description={repository.description} />
         <LanguageBar languagesUrl={repository.languages_url} />
       </CardContent>
       <CardContent>
-        <Typography variant='body2' component='p' style={{color: '#5e707a'}}>
-          {repository.topics
-            .filter(topic => topic !== 'portfolio')
-            .map(topic =>
-              <span key={topic}>#{topic} </span>
-            )}
-        </Typography>
+        <ProjectTopics topics={repository.topics} />
       </CardContent>
     </Card>
   </Grid>
