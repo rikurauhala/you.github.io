a7821e1544b7f2484ddc0115b1a293013482e68f
Riku Rauhala <riku@rauhala.io>
2022-08-24 22:03:47 +0300

    Remove redundant component Subtitle

--- src/components/Subtitle.tsx
+++
@@ -1,9 +0,0 @@
-import { Typography } from '@mui/material';
-
-const Subtitle = ({ subtitle }: { subtitle: string }): JSX.Element => (
-  <Typography gutterBottom component='h1' variant='h5'>
-    {subtitle}
-  </Typography>
-);
-
-export default Subtitle;
