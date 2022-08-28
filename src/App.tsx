c1b0da6df3a0a370d9366e4e6c7211b6c25acd21
Riku Rauhala <riku@rauhala.io>
2022-08-28 18:21:59 +0300

    Refactor the way components are imported in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import { useEffect } from 'react';
 
 import { Container } from '@mui/material';
 
-import Info from './components/Info/Info';
-import About from './components/About/About';
-import Projects from './components/Projects/Projects';
-import Footer from './components/Footer/Footer';
+import Info from './components/Info';
+import About from './components/About';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
 
 import { setTitle } from './utils/helper';
 
