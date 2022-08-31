18805f35342d89562fda18bdbe1f3c86ef5e8dd7
Riku Rauhala <riku@rauhala.io>
2022-08-31 19:34:39 +0300

    Refactor SearchBar.tsx

--- src/components/Projects/SearchBar.tsx
+++ src/components/Projects/SearchBar.tsx
@@ -11,23 +11,26 @@ const SearchBar = ({ setState }: { setState: (arg0: string) => void }) => {
     backgroundColor: '#051222',
     border: '#0f3552 1px solid',
     display: 'flex',
+    margin: '15px 0',
     padding: '2px 4px'
   };
 
   return (
-    <div style={{margin: '15px 0'}}>
-      <Paper component='form' style={style}>
-        <InputBase
-          inputProps={{ 'aria-label': 'Search' }}
-          onChange={(event) => setState(event.target.value)}
-          placeholder='Search'
-          sx={{ color: '#fff', flex: 1, ml: 1 }}
-        />
-        <IconButton aria-label='search' sx={{ color: '#fff', padding: '10px' }} type='button'>
-          <SearchIcon />
-        </IconButton>
-      </Paper>
-    </div>
+    <Paper component='form' style={style}>
+      <InputBase
+        inputProps={{ 'aria-label': 'Search' }}
+        onChange={(event) => setState(event.target.value)}
+        placeholder='Search'
+        sx={{ color: '#fff', flex: 1, ml: 1 }}
+      />
+      <IconButton
+        aria-label='search'
+        sx={{ color: '#fff', padding: '10px' }}
+        type='button'
+      >
+        <SearchIcon />
+      </IconButton>
+    </Paper>
   );
 };
 
