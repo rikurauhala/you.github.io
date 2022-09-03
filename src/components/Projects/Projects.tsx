761effab270c510c0048174f6ab40da9b9172ac6
Riku Rauhala <riku@rauhala.io>
2022-09-03 21:36:47 +0300

    Fix formatting

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -77,7 +77,7 @@ const Projects = (): JSX.Element => {
       {
         filteredRepositories.length > 0
           ? <ProjectGrid repositories={filteredRepositories} />
-          : <Typography paragraph component='p' variant='body1'> No projects yet!</Typography>
+          : <Typography paragraph component='p' variant='body1'>No projects yet!</Typography>
       }
     </>
   );
