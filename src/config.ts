dfeb0071947f8faddb520f8a87b4c73ffd7842b3
Riku Rauhala <riku@rauhala.io>
2022-09-04 22:04:52 +0300

    Remove repository environment variable as redundant

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,4 @@
-export const keyword = process.env.REACT_APP_KEYWORD;
-export const name = process.env.REACT_APP_NAME || 'Firstname Lastname';
-export const repository = process.env.REACT_APP_REPOSITORY || 'https://github.com/username/repository';
 export const username = process.env.REACT_APP_USERNAME || 'username';
-export const url = process.env.REACT_APP_URL || repository;
+export const name = process.env.REACT_APP_NAME || username;
+export const keyword = process.env.REACT_APP_KEYWORD;
+export const url = process.env.REACT_APP_URL || `${username}.github.io`;
