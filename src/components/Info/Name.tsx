cd56e3f141b83a5f7e13940afbb76ce525cd9490
Riku Rauhala <riku@rauhala.io>
2022-09-05 21:20:50 +0300

    Display username if the profile has no name

--- src/components/Info/Name.tsx
+++ src/components/Info/Name.tsx
@@ -1,8 +1,10 @@
 import Typography from '@mui/material/Typography';
 
+import { username } from '../../config';
+
 const Name = ({ name }: { name: string }): JSX.Element => (
   <Typography variant='h4' component='h1'>
-    {name}
+    {name ? name : username}
   </Typography>
 );
 
