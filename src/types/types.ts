15eac4d0cdfe9cf91831be42c2f29565259596f2
Riku Rauhala <riku@rauhala.io>
2022-09-07 10:52:41 +0300

    Refactor type User

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,4 +1,4 @@
-export interface License {
+interface License {
   key: string
   name: string
   node_id: string
@@ -99,23 +99,26 @@ export interface RepositoryFull {
   web_commit_signoff_required: boolean
 }
 
-type ToBeOmitted =
-  | 'bio'
-  | 'blog'
-  | 'company'
-  | 'created_at'
-  | 'email'
-  | 'followers'
-  | 'following'
-  | 'hireable'
-  | 'location'
-  | 'name'
-  | 'public_gists'
-  | 'public_repos'
-  | 'twitter_usename'
-  | 'updated_at';
-
-export type User = Omit<UserFull, ToBeOmitted>;
+interface User {
+  avatar_url: string
+  events_url: string
+  followers_url: string
+  following_url: string
+  gists_url: string
+  gravatar_id: string
+  html_url: string
+  id: number
+  login: string
+  node_id: string
+  organizations_url: string
+  received_events_url: string
+  repos_url: string
+  site_admin: boolean
+  starred_url: string
+  subscriptions_url: string
+  type: string
+  url: string
+}
 
 export interface UserFull {
   avatar_url: string
