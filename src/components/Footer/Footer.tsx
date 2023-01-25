a4cd7d07e82456f1ed346b5520e104767cbf55f1
Riku Rauhala <riku@rauhala.io>
2023-01-25 19:22:38 +0200

    Improve Footer styling

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,10 +6,9 @@ import Grid from '@mui/material/Grid';
 
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import HomeIcon from '@mui/icons-material/Home';
 import PersonIcon from '@mui/icons-material/Person';
 
-import { name, url, username } from '../../config';
+import { name, username } from '../../config';
 
 export interface FooterButtonProps {
   icon: ReactElement
@@ -18,7 +17,7 @@ export interface FooterButtonProps {
 }
 
 const FooterButton = ({ icon, text, url }: FooterButtonProps) => (
-  <Grid item md={3} textAlign="center" xs={6}>
+  <Grid item md={4} textAlign="center" xs={12}>
     <Button
       href={url}
       size="small"
@@ -31,24 +30,16 @@ const FooterButton = ({ icon, text, url }: FooterButtonProps) => (
 );
 
 const Footer = () => {
-  let address = url;
-  if (!url) address = `${username}.github.io`;
   const source = `https://github.com/${username}/${username}.github.io`;
 
   return (
-    <Container sx={{ margin: '20px 0px' }} >
+    <Container disableGutters sx={{ margin: '20px 0px' }} >
       <Grid
-        alignItems="center"
         container
         direction={{ xs: 'column', sm: 'row' }}
-        justifyContent="center"
-        spacing={{ xs: 1, sm: 0.5, md: 0 }}
+        justifyContent="space-between"
+        spacing={{ xs: 1, md: 2 }}
       >
-        <FooterButton
-          icon={<HomeIcon />}
-          text={address}
-          url=""
-        />
         <FooterButton
           icon={<PersonIcon />}
           text={name}
