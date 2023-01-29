4736cbe4343fc10c590f84fbe9a93e764b3bdf97
Riku Rauhala <riku@rauhala.io>
2023-01-29 14:03:41 +0200

    Use a Stack element for the detail buttons

--- src/components/Info/Details.tsx
+++ src/components/Info/Details.tsx
@@ -1,3 +1,5 @@
+import Stack from '@mui/material/Stack';
+
 import Company from './Company';
 import Location from './Location';
 import GitHub from './GitHub';
@@ -11,12 +13,12 @@ interface DetailsProps {
 }
 
 const Details = ({ company, location, github, twitter }: DetailsProps): JSX.Element => (
-  <>
+  <Stack direction="row" justifyContent="center" spacing={1}>
     {company && <Company company={company} />}
     {location && <Location location={location} />}
     {github && <GitHub username={github} />}
     {twitter && <Twitter username={twitter} />}
-  </>
+  </Stack>
 );
 
 export default Details;
